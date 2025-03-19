e5a1b7a2bff903c63279ff0a6baa13a2f3e8829f
kjelltillstrand <kjell@tillstrand.com>
2025-03-20 00:34:18 +0100

    inital entry

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,13 @@
-// playwright.config.js
+  // playwright.config.js
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-    timeout: 30000,
     use: {
+      baseURL: process.env.BASE_URL || 'http://localhost:3000', // fallback if not deployed
       headless: true,
       viewport: { width: 1280, height: 720 },
       ignoreHTTPSErrors: true,
     },
+    timeout: 30000,
   };
   module.exports = config;
   
