526b53c851e2836e0603b11dbcd92eca8f58e631
KjellTillstrand <KjellTillstrand@users.noreply.github.com>
2025-03-21 12:41:41 +0000

    deploy: 7b5edce1a922f15f620c56ef71a227964c3127f1

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
     use: {
-      baseURL: process.env.BASE_URL || 'http://localhost:3000', // fallback if not deployed
+      baseURL: process.env.BASE_URL || 'http://kjelltillstrand.github.io/padeltournament', // fallback if not deployed
       headless: true,
       viewport: { width: 1280, height: 720 },
       ignoreHTTPSErrors: true,
