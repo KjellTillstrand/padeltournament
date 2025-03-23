798cf31ca9dcb9c8154b8ecf5bf9b015e0122a01
kjelltillstrand <kjell@tillstrand.com>
2025-03-23 09:13:57 +0100

    inital entry

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,13 @@
-  // playwright.config.js
+// playwright.config.js
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
-    use: {
-      baseURL: process.env.BASE_URL || 'http://kjelltillstrand.github.io/padeltournament', // fallback if not deployed
-      headless: true,
-      viewport: { width: 1280, height: 720 },
-      ignoreHTTPSErrors: true,
-    },
-    timeout: 30000,
-  };
-  module.exports = config;
-  
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  use: {
+    baseURL: process.env.BASE_URL || 'http://kjelltillstrand.github.io/padeltournament', // fallback if not deployed
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+  },
+  timeout: 30000,
+};
+module.exports = config;
