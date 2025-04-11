da88937ab03e44a30c85565ccfb107f5f7821b72
kjelltillstrand <kjell@tillstrand.com>
2025-04-11 10:27:15 +0200

    inital entry

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,7 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    url: process.env.BASE_URL || 'http://localhost:3000', // fallback if not deployed
+    baseURL: process.env.BASE_URL || 'http://localhost:3000', // fallback if not deployed
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
